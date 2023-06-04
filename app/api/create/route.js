5f037297715d4d64f7b41deb92501a3c89912f71
Omkar <omkarkamble1235@gmail.com>
2023-06-05 04:04:00 +0530

    final commit

--- app/api/create/route.js
+++ app/api/create/route.js
@@ -1,43 +1,50 @@
-import { customAlphabet } from 'nanoid'
+import { customAlphabet } from 'nanoid';
 import { query } from '@/lib/dbconnect';
 
 export async function POST(request) {
 	const result = await request.json();
 	let { link, id: uuid } = result;
-	
-	if (link.indexOf("http://") == 0 || link.indexOf("https://") == 0) {
-       
-    } else {
-		link = `http://${link}`
+
+	if (link.indexOf('http://') == 0 || link.indexOf('https://') == 0) {
+	} else {
+		link = `http://${link}`;
 	}
 
-	const nanoid = customAlphabet('1234567890abcdefghijklmnopqrstuvwxyz')
-	const id = nanoid(6)
+	const nanoid = customAlphabet('1234567890abcdefghijklmnopqrstuvwxyz');
+	let id = nanoid(6);
+	if (id === 'create' || id === 'login') {
+		id = nanoid(6);
+	}
 	const shortLink = `ulinkk.vercel.app/${id}`;
 
 	const now = new Date();
 	const created_at = `${now.getDate()}-${now.getMonth()}-${now.getFullYear()} ${now.getHours()}:${now.getMinutes()}`;
-	
-	const { rows } = await query('INSERT INTO public.links (original_link, code, short_link, is_monitized, created_at, user_id) VALUES ($1, $2, $3, $4, $5, $6)', [link, id, shortLink, false, created_at, uuid], (err) => {
-		if (err) {
-			return new Response(JSON.stringify({
-				success: false
-			}),
-			{
-				status: 400
-			}
-			);
-		}}
-	);
-	
-	return new Response(JSON.stringify({
-		success: true,
-		shortLink,
-		id
-	}),
-	{
-		status: 200
-	}
-	);
-};
 
+	const { rows } = await query(
+		'INSERT INTO public.links (original_link, code, short_link, is_monitized, created_at, user_id) VALUES ($1, $2, $3, $4, $5, $6)',
+		[link, id, shortLink, false, created_at, uuid],
+		(err) => {
+			if (err) {
+				return new Response(
+					JSON.stringify({
+						success: false
+					}),
+					{
+						status: 400
+					}
+				);
+			}
+		}
+	);
+
+	return new Response(
+		JSON.stringify({
+			success: true,
+			shortLink,
+			id
+		}),
+		{
+			status: 200
+		}
+	);
+}
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,65 +1,90 @@
-"use client";
+'use client';
 
 import { signIn, signOut, useSession } from 'next-auth/react';
 import { useEffect, useState } from 'react';
 import { BeatLoader } from 'react-spinners';
 
 function Navbar() {
-	const [popup, setPopup] = useState(false)
-	const [isLoggedIn, setIsLoggedIn] = useState(false)
-	const [username, setUsername] = useState('')
-	const [isFetching, setIsFetching] = useState(false)
+	const [popup, setPopup] = useState(false);
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
+	const [username, setUsername] = useState('');
+	const [isFetching, setIsFetching] = useState(false);
 
 	const handerLogout = () => {
-		localStorage.removeItem('data')
-		.replace('/')
-		setIsLoggedIn(false)
-	}
+		localStorage.removeItem('data').replace('/');
+		setIsLoggedIn(false);
+	};
 
 	const { data } = useSession();
 
 	useEffect(() => {
-		setIsFetching(true)
+		setIsFetching(true);
 		if (data && data.user) {
-			setIsLoggedIn(true)
-			setUsername(data.user.username ? data.user.username : '')
-			setIsFetching(false)
+			setIsLoggedIn(true);
+			setUsername(data.user.username ? data.user.username : '');
+			setIsFetching(false);
 		} else {
-			setIsFetching(false)
+			setIsFetching(false);
 		}
-	})
+	});
 
 	return (
-	<nav
-	onClick={() => isLoggedIn ? setPopup(!popup) : null}
-	className={`hover:bg-opacity-70 z-20 bg-white backdrop-blur bg-opacity-60 absolute top-2 right-5 w-52 ${!popup ? 'h-10' : 'h-[7.2rem]'} rounded-[1.3rem] flex flex-col px-2 cursor-pointer py-1 transition-all overflow-hidden select-none`} >
-		{isFetching ?
-		<div className='flex items-center justify-center h-10'>
-		<BeatLoader color='#707070' size={'8px'}/> 
-		</div>
-		: 
-		<>
-		{isLoggedIn ? 
-			<>
-				<div className='w-full flex-row flex items-center justify-between'>
-					<h2 className='font-semibold ml-2 text-neutral-800 truncate'>{username}</h2>
-					<img className='w-8 bg-white rounded-full' src={`https://robohash.org/${username}?set=set3`} />
+		<nav
+			onClick={() => (isLoggedIn ? setPopup(!popup) : null)}
+			className={`hover:bg-opacity-70 z-20 bg-white backdrop-blur bg-opacity-60 absolute top-2 right-5 w-52 ${
+				!popup ? 'h-10' : 'h-[9.5rem]'
+			} rounded-[1.3rem] flex flex-col px-2 cursor-pointer py-1 transition-all overflow-hidden select-none`}
+		>
+			{isFetching ? (
+				<div className="flex items-center justify-center h-10">
+					<BeatLoader color="#707070" size={'8px'} />
 				</div>
-				<div className='mt-1 gap-1 w-full flex flex-col text-center'>
-					<a href={`/u/${username}`}><h3 className='hover:bg-white hover:bg-opacity-60 rounded-xl py-1 z-50'>My links</h3></a>
-					<h3 onClick={() => signOut({ redirect: true })} className='hover:bg-white hover:bg-opacity-60 rounded-xl py-1'>Logout</h3>
-				</div>
-			</>
-			:
-			<div className='w-full flex flex-col text-center'>
-				<h3 onClick={() => signIn()} className='hover:bg-white hover:bg-opacity-60 rounded-2xl py-1 z-50 cursor-pointer'>Login</h3>
-			</div>
-			}	
-		</>
-		}
-		
-	</nav>
-  )
+			) : (
+				<>
+					{isLoggedIn ? (
+						<>
+							<div className="w-full flex-row flex items-center justify-between">
+								<h2 className="font-semibold ml-2 text-neutral-800 truncate">
+									{username}
+								</h2>
+								<img
+									className="w-8 bg-white rounded-full"
+									src={`https://robohash.org/${username}?set=set3`}
+								/>
+							</div>
+							<div className="mt-1 gap-1 w-full flex flex-col text-center">
+								<a href={`/u/${username}`}>
+									<h3 className="hover:bg-white hover:bg-opacity-60 rounded-2xl py-1 z-50">
+										My links
+									</h3>
+								</a>
+								<a href={`/create`}>
+									<h3 className="hover:bg-white hover:bg-opacity-60 rounded-2xl py-1 z-50">
+										Create
+									</h3>
+								</a>
+								<h3
+									onClick={() => signOut({ redirect: true })}
+									className="hover:bg-white hover:bg-opacity-60 rounded-2xl py-1"
+								>
+									Logout
+								</h3>
+							</div>
+						</>
+					) : (
+						<div className="w-full flex flex-col text-center">
+							<h3
+								onClick={() => signIn()}
+								className="hover:bg-white hover:bg-opacity-60 rounded-2xl py-1 z-50 cursor-pointer"
+							>
+								Login
+							</h3>
+						</div>
+					)}
+				</>
+			)}
+		</nav>
+	);
 }
 
-export default Navbar
+export default Navbar;
